37d73658fe07feaebffaeee20e6ef6bb768e0357
Vadim <bicherov.vv@phystech.edu>
2022-09-22 23:29:19 +0300

    add files with homework

---
+++ homework1/task 3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	float x, y;
+
+	std::cout << "Введите x: ";
+	std::cin >> x;
+
+	std::cout << "Введите y: ";
+	std::cin >> y;
+
+	x += y;
+	y = x - y;
+	x -= y;
+
+	std::cout << "Результат смены: ";
+	std::cout << "x = " << x;
+	std::cout << ", y = " << y << "\n";
+
+	return 0;
+}
